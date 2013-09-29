65fbb3cc8f787aa27e9b755d0bca2d1b8a5851ec
Чечеткин Илья <illaech@gmail.com>
2013-09-30 00:14:31 +0400

    bat-файл для копирования hed* в нужную директорию
    Note: при запуске должен находиться в папке с копируемыми файлами
    
    P.S. Не уверен насчет кнопки "Update formats" -- может ли установиться
    без ее нажатия. Необходима проверка

---
+++ utils/install.bat
@@ -0,0 +1,48 @@
+@echo off
+cd %programfiles%
+
+<nul set /p str="Searching for MikTeX directory...    "
+if exist Miktex* (
+    goto search
+) else (
+    echo [FAIL]
+    echo Exiting...
+    goto exit
+)
+
+:search
+for /f "delims=" %%i in ('dir /ad /b ^^ findstr /r "MikTeX*"') do set mp=%programfiles%\%%i
+set hedd=%mp%\tex\latex\hed
+echo [DONE]
+
+if exist "%hedd%" (
+    echo HEd folder exists...
+    goto copy
+) else (
+    <nul set /p str="Creating HEd directory...            "
+    md "%hedd%" > nul
+    if exist "%hedd%" (
+        echo [DONE]
+        goto copy
+    ) else (
+        echo [FAIL]
+        echo Can't create folder! Exiting...
+        goto exit
+    )
+)
+
+:copy
+<nul set /p str="Copying files to MikTeX...           "
+cd %~dp0
+copy /Y hed* "%hedd%" > nul
+if %ERRORLEVEL% == 0 (
+    echo [DONE]
+    echo.
+    echo Go to MikTeX Settings and push the "Update Formats" and "Refresh FNDB" buttons
+) else (
+    echo [FAIL]
+    echo Can't copy files into directory! Exiting...
+)
+
+:exit
+pause
