59916c259cac80030d67f99a5aded78cdee3488c
Чечеткин Илья <illaech@gmail.com>
2013-10-24 14:45:11 +0400

    [install] Установщик на кириллице + обновление readme

--- utils/install.bat
+++ utils/install.bat
@@ -2,54 +2,55 @@
 cd %programfiles%
 set /a ex=0
 
-<nul set /p str="Searching for MikTeX directory...    "
+<nul set /p str="Поиск директории MikTeX...               "
 if exist Miktex* (
     goto search
 ) else (
-    echo [FAIL]
-    echo Exiting...
+    echo [ПРОВАЛ]
+    echo Директория не найдена. Выход...
     goto exit
 )
 
 :search
 for /f "delims=" %%i in ('dir /ad /b ^^ findstr /r "MikTeX*"') do set mp=%programfiles%\%%i
 set hedd=%mp%\tex\latex\hed
-echo [DONE]
+echo [ГОТОВО]
 
 if exist "%hedd%" (
-    echo HEd folder exists...
+    echo Директория HEd существует
     set /a ex=1
     goto copy
 ) else (
-    <nul set /p str="Creating HEd directory...            "
+    <nul set /p str="Создание директории для HEd...           "
     md "%hedd%" > nul
     if exist "%hedd%" (
-        echo [DONE]
+        echo [ГОТОВО]
         goto copy
     ) else (
-        echo [FAIL]
-        echo Can't create folder! Exiting...
+        echo [ПРОВАЛ]
+        echo Невозможно создание новой папки! Выход...
         goto exit
     )
-)
+)        
 
 :copy
-<nul set /p str="Copying files to the directory...    "
+<nul set /p str="Копирование файлов...                    "
 cd %~dp0
 cd ..\source
 copy /Y hed* "%hedd%" > nul
 if %ERRORLEVEL% == 0 (
-    echo [DONE]
+    echo [ГОТОВО]
     echo.
     if %ex%==0 (
-       echo Go to the MikTeX Settings and push the "Refresh FNDB" button
+       echo Теперь нажмите "Refresh FNDB" в MikTeX Settings
+       timeout /t 5 /nobreak > nul
     ) else (
-       echo Go back to work!
+       echo Обновление успешно установлено
     )
 ) else (
-    echo [FAIL]
-    echo Can't copy files into the directory! Exiting...
+    echo [ПРОВАЛ]
+    echo Невозможно скопировать файлы! Выход...
 )
 
 :exit
-pause
+timeout /t 3 /nobreak > nul
