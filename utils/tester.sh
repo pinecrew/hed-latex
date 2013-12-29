63ff37a66ce57a02405908d267c4ed6ac4566562
Vova Abdrakhmanov <369565@gmail.com>
2013-12-29 18:48:47 +0400

    update tests

--- utils/tester.sh
+++ utils/tester.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
 ## preparation
-[ -d ../tmp ] && rm -r ../tmp
-mkdir -p ../{tmp,build}
-cp ../source/* ../tmp
-cp  -r ../tests/* ../tmp
-tests=$(ls ../tests/*.tex)
-
+base_dir="$(pwd -P)/../"
+[ -d $base_dir/tmp ] && rm -r $base_dir/tmp
+mkdir -p $base_dir/{tmp,build}
+cp  -r $base_dir/tests/* $base_dir/tmp
+tests=$(ls ../tests/)
+i=1
+for t in $tests; do
+    echo "$i) $t"
+    let i=$i+1
+done
+echo "Выберите, какие тесты проводить:"
+read selected
 cd ../tmp
-for test in $tests; do
-    pdffile=$(echo $test | sed -e "s/tests/tmp/;s/\.tex/.pdf/")
-    latexmk -pdf $test && mv $pdffile ../build
+i=1
+for t in $tests; do
+    for j in $selected; do
+        if [[ i -eq j ]]; then
+            cd $t &&\
+            cp ../../source/* .
+            latexmk -pdf "test.tex" &&\
+            mv test.pdf ../../build/$t.pdf &&\
+            cd ..
+        fi
+    done
+    let i=$i+1
 done
 
